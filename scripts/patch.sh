14c75dc63f7c6f198871763c5eaaaf517efda1cb
AuTsing <763025696@qq.com>
2022-04-27 23:25:39 +0800

    cat patch

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -5,6 +5,7 @@ workspace=$(realpath "$(dirname "$0")"/../)
 cd "$workspace/node"
 patches=$(find $workspace/patches/$NODE_VERSION -name \*.patch)
 for patch in $patches; do
-    echo "Applying patch: $(basename "$patch")"
+    echo "Applying patch: $patch"
     patch -p1 <"$patch"
 done
+cat "./android-configure"
